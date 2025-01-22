3b8fd53db0f72ef08fc2d8055f0e6b567c2bc15e
kominho1205 <mint4576@icloud.com>
2025-01-23 08:20:31 +0900

    커밋 메시지

--- server.js
+++ server.js
@@ -29,8 +29,9 @@ httpServer.listen(80, () => {
 });
 
 app.use((req, res, next) => {
-    res.header('Cache-Control', 'no-store'); // 캐시 비활성화
+    res.header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     res.header('Pragma', 'no-cache');
     res.header('Expires', '0');
+    res.header('Surrogate-Control', 'no-store');
     next();
 });
