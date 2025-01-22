3cc5389841628322ec0371385798d26ec031e2b7
kominho1205 <mint4576@icloud.com>
2025-01-23 08:22:52 +0900

    커밋 메시지

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const http = require('http'); // HTTPS는 사용하지 않음
 
 const app = express();
 
+// ETag 비활성화
+app.disable('etag'); // ETag 비활성화
+
 // CORS 설정 (모든 요청을 허용)
 const cors = require('cors');
 app.use(cors()); // 개발 중에는 모든 요청 허용
@@ -12,6 +15,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // 모든 출처 허용
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // 허용 메서드
     res.header('Access-Control-Allow-Headers', 'Content-Type'); // 허용 헤더
+    res.header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate'); // 캐시 비활성화
+    res.header('Pragma', 'no-cache'); // HTTP 1.0 캐시 비활성화
+    res.header('Expires', '0'); // 캐시 만료
     next();
 });
 
@@ -26,12 +32,4 @@ const httpServer = http.createServer(app);
 // HTTP 서버 실행 (80번 포트에서 실행)
 httpServer.listen(80, () => {
     console.log('HTTP Server running on http://localhost:80');
-});
-
-app.use((req, res, next) => {
-    res.header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-    res.header('Pragma', 'no-cache');
-    res.header('Expires', '0');
-    res.header('Surrogate-Control', 'no-store');
-    next();
 });
