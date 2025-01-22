b7cf9174cf283695c2794fd6df0ab1f334ecc57f
kominho1205 <mint4576@icloud.com>
2025-01-23 07:31:20 +0900

    커밋 메시지

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const app = express();
 const cors = require('cors');
 app.use(cors()); // 개발 중에는 모든 요청 허용
 
+// 모든 요청에 공통 헤더 추가
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // 모든 출처 허용
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // 허용 메서드
+    res.header('Access-Control-Allow-Headers', 'Content-Type'); // 허용 헤더
+    next();
+});
+
 // 기본 라우트
 app.get('/', (req, res) => {
     res.status(200).contentType('text/plain').send('Hello from the HTTP server!');
